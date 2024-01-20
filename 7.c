805d32f39eaef7f55d00b4d9e7f13ec0c5155376
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-21 00:00:07 +0530

    7func

--- 7.c
+++ 7.c
@@ -1,4 +1,4 @@
-//Reverse element frm array
+/*//Reverse element frm array
 #include<stdio.h>
 int main(){
     int arr[10],n,i;
@@ -21,7 +21,36 @@ int main(){
     for(int i=0;i<n;i++)
     printf("%d ",arr[i]);
     return 0;
+}*/
+
+//Using functions
+
+
+#include<stdio.h>
+void reverseElement(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        int temp=arr[start];   //Swapping elements
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+
 }
+int main(){
+    int arr[10],n,i;
+    printf("Enter size of array:");
+    scanf("%d",&n);
+    printf("Enter elements of an array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    reverseElement(arr, n);
+    printf("the rversed array is:");
+    for(int i=0;i<n;i++)
+    printf("%d ",arr[i]);
+    return 0;
 
-
-
+}
