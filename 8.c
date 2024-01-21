43b919c1a09c38b890189d61be1facafb6029c8a
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-21 10:06:20 +0530

    8

---
+++ 8.c
@@ -0,0 +1,9 @@
+// to Find the Number of Elements in an Array
+#include<stdio.h>
+int main(){
+        int arr[]={7,86 ,9,3,5,23,64};
+        int n;
+        n=sizeof(arr);
+        printf("Size of given array is %d\n",n/sizeof(int));
+        return 0;
+}
