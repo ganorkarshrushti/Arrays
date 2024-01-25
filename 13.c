63c0edf71e17fa968f86aceaf94afbe7916bd4a1
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-25 21:36:17 +0530

    13

---
+++ 13.c
@@ -0,0 +1,21 @@
+//to find the sum of all eleme* pointers as arguments.
+#include<stdio.h>
+int addnum(int *ptr){
+    int index,total=0;
+    for(index=0; index<5; index++){
+        total=total+ *(ptr+index);
+    }
+    return(total);
+  
+}
+
+int  main(){
+    int arr[5]={3,4,6,7,8};
+    int sum;
+    //int addnum(int *ptr);
+    sum=addnum(arr);
+    printf("Sum of all array elements = %5d\n", sum);
+    return 0;
+}
+
+    
