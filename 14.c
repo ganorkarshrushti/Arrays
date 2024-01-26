4db6d96a5ba33ad7d3735a46f9a464e54bf5f96e
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-26 21:10:30 +0530

    14

---
+++ 14.c
@@ -0,0 +1,26 @@
+//to accept N numbers and arrange them in an ascending order (kind of bubble sort only)
+#include<stdio.h>
+int main(){
+    int n,i,j,arr[10],a;
+    printf("Enter the value of N");
+    scanf("%d",&n);
+    printf("Enter the number ");
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++){      // represents each pass through the list
+        for(j=0;j<n;j++){    // compares adjacent elements and swaps them if necessary
+            if (arr[i] > arr[j]) 
+                {
+                    a=arr[i];    //current elem ,is greater than the element being compared (arr[j]), and if true, the elements are swapp
+                    arr[i]=arr[j];
+                    arr[j]=a;
+                }
+        }
+    }
+    printf("The numbers arranged in ascending order are ");
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+
+
+}
