e3e74d66155c20f70e618c3197350efd5f0fbe96
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-27 20:01:13 +0530

    17

--- 17.c
+++ 17.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     int n,i,j,arr[10];
-    printf("ENter size of array:");
+    printf("Enter the size of array:");
     scanf("%d",&n);
-    printf("ENter elemnts of array:");
+    printf("Enter elements of array:");
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
@@ -15,9 +15,36 @@ int main(){
     
     printf("Odd no.s in array are:\n");
     for(int i=0;i<n;i++){
-        if(arr[i]%2==1)
+        if(arr[i]%2 == 1)
             printf("%d\n",arr[i]);
         }
     }
+*/
+
+#include<stdio.h>
+void EvenOdd(int arr[],int n){
+    printf("Even no.s in array are:\n");
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0)
+            printf("%d\n",arr[i]);
+    }
     
+    printf("Odd no.s in array are:\n");
+    for(int i=0;i<n;i++){
+        if(arr[i]%2 == 1)
+            printf("%d\n",arr[i]);
+        }
+    }
+    int main(){
+        int n,i,arr[10];
+        printf("Enter the size of array:");
+        scanf("%d",&n);
+        printf("Enter elements of array:");
+        for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    EvenOdd(arr,n);
+    return 0;
+
+    }
     
