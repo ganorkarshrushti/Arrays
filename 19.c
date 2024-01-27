c37a91d05920f450057b3cf0c8412f6290cd7f4d
Shrushti Ganorkar <shrushtiganorkar83@gmail.com>
2024-01-27 22:06:31 +0530

    19

--- 19.c
+++ 19.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-
+//to print alternate elements of an array .
+/*#include <stdio.h>
 int main() {
     int n, i, arr[10];
 
@@ -11,8 +11,32 @@ int main() {
         scanf("%d", &arr[i]);
 
     printf("Alternate elements of the given array:\n");
-    for (i = 0; i < n; i = i + 2)  //i=i+2 for gettimg an alternate sequence
+    for (i = 0; i < n; i = i + 2)   //i=i+2 for gettimg an alternate sequence
         printf("%d\n", arr[i]);
 
     return 0;
 }
+*/
+//using function 
+#include<stdio.h>
+int  AlternateElement(int arr[],int n){
+        printf("Alternate elements of the given array:\n");
+        for (int i = 0; i < n; i = i + 2)   //i=i+2 for gettimg an alternate sequence
+        printf("%d\n", arr[i]);
+}
+int main(){
+
+    int n, i, arr[10];
+    printf("Enter size of an array: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of an array:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    AlternateElement(arr, n);
+    return 0;
+}
+
+
+
+
